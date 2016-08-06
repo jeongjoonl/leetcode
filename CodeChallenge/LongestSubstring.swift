6785e9b7736ac507e193c0cf802c0cf9ed0a2da9
Jeongjoon Lee <joondev32@gmail.com>
2016-08-06 15:40:23 -0700

    Longest Substring initial setup

---
+++ CodeChallenge/LongestSubstring.swift
@@ -0,0 +1,17 @@
+//
+//  LongestSubstring.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/6/16.
+//
+//
+
+/// Given a string, find the length of the longest substring without repeating characters.
+///
+/// Examples:
+/// Given "abcabcbb", the answer is "abc", which the length is 3.
+/// Given "bbbbb", the answer is "b", with the length of 1.
+/// Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+func lengthOfLongestSubstring(s: String) -> Int {
+	return 0
+}
