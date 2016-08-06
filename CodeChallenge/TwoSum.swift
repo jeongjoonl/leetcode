a4a57eb35f1b4dd1379c310431fb229f12347386
Jeongjoon Lee <joondev32@gmail.com>
2016-08-05 19:17:40 -0700

    Implemented TwoSum problem

---
+++ CodeChallenge/TwoSum.swift
@@ -0,0 +1,41 @@
+//
+//  TwoSum.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/5/16.
+//
+//
+
+import Foundation
+
+/// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+/// You may assume that each input would have exactly one solution.
+///
+/// Example:
+/// Given nums = [2, 7, 11, 15], target = 9,
+///
+/// Because nums[0] + nums[1] = 2 + 7 = 9,
+/// return [0, 1].
+///
+/// - parameter nums:   list of arbitrary numbers
+/// - parameter target: arbitrary numbers
+///
+/// - returns: indices of the two numbers in list that sum up to target number
+func twoSum(nums: [Int], target: Int) -> [(Int, Int)] {
+	var answers = [(Int, Int)]()
+
+	// Create dictionary that maps number to index
+	var dict = [Int:Int]()
+
+	for i in 0..<nums.count {
+		let num = nums[i]
+
+		dict[num] = i
+
+		if let j = dict[target - num] {
+			answers.append( (j, i) )
+		}
+	}
+
+	return answers
+}
