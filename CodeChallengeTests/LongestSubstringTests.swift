7b2583f069268c177f6912b3f0a62043b0280a80
Jeongjoon Lee <joondev32@gmail.com>
2016-08-06 17:44:59 -0700

    Initial set up for LongestSubstring test case

---
+++ CodeChallengeTests/LongestSubstringTests.swift
@@ -0,0 +1,15 @@
+//
+//  LongestSubstringTests.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/6/16.
+//
+//
+
+import XCTest
+
+class LongestSubstringTests: XCTestCase {
+	func testLengthOfLongestSubstring() {
+		
+	}
+}
