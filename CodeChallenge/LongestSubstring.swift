46af460428419b6148a5bb06fbe9b7c8036d6972
Jeongjoon Lee <joondev32@gmail.com>
2016-08-06 18:56:05 -0700

    Implemented Length Of Longest Substring

--- CodeChallenge/LongestSubstring.swift
+++ CodeChallenge/LongestSubstring.swift
@@ -12,6 +12,34 @@
 /// Given "abcabcbb", the answer is "abc", which the length is 3.
 /// Given "bbbbb", the answer is "b", with the length of 1.
 /// Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+///
+/// - parameter s: sequence of arbitrary characters
+///
+/// - returns: length of the longest substring without repeating characters
 func lengthOfLongestSubstring(s: String) -> Int {
-	return 0
+
+	var cut = 0, longestLength = 0
+	var indexMap = [Character:Int]()
+
+	for index in s.characters.indices {
+		let key = s[index]
+		let i = s.distance(from: s.startIndex, to: index)
+
+		if let val = indexMap[key] {
+			if val >= cut {
+				// Update cut
+				cut = val + 1
+			}
+		}
+
+		// Add/Update mapping
+		indexMap[key] = i
+
+		// Update longest length
+		let curLength = i - cut + 1
+		longestLength = max(longestLength, curLength)
+
+	}
+
+	return longestLength
 }
