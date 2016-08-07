0ac1652febe2c4214c2295e5c6d84bb8e61e6c23
Jeongjoon Lee <joondev32@gmail.com>
2016-08-07 14:20:48 -0700

    Initial setup for median of Two sorted array problems

---
+++ CodeChallenge/MedianOfTwoSortedArray.swift
@@ -0,0 +1,27 @@
+//
+//  MedianOfTwoSortedArray.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/7/16.
+//
+//
+
+/// Find the median of the two sorted arrays.
+///
+/// Example 1:
+/// nums1 = [1, 3]
+/// nums2 = [2]
+/// The median is 2.0
+///
+/// Example 2:
+/// nums1 = [1, 2]
+/// nums2 = [3, 4]
+/// The median is (2 + 3)/2 = 2.5
+///
+/// - parameter nums1: sorted array
+/// - parameter nums2: sorted array
+///
+/// - returns: median of the two sorted arrays
+func findMedianSortedArrays(nums1: [Int], nums2: [Int]) -> Double {
+	return 0
+}
---
+++ CodeChallengeTests/MedianOfTwoSortedArrayTests.swift
@@ -0,0 +1,14 @@
+//
+//  MedianOfTwoSortedArrayTests.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/7/16.
+//
+//
+
+import XCTest
+
+class MedianOfTwoSortedArrayTests: XCTestCase {
+	func testFindMedianSortedArrays() {
+	}
+}
