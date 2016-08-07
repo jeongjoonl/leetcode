e3342f8dba8e5f4d798ffb3ea63becee2b92e675
Jeongjoon Lee <joondev32@gmail.com>
2016-08-06 17:53:20 -0700

    Added test case for lengthOfLongestSubstring

--- CodeChallengeTests/LongestSubstringTests.swift
+++ CodeChallengeTests/LongestSubstringTests.swift
@@ -10,6 +10,28 @@ import XCTest
 
 class LongestSubstringTests: XCTestCase {
 	func testLengthOfLongestSubstring() {
-		
+		// String: "abcabcddd", Substring "abc" -> 3
+		var result = lengthOfLongestSubstring(s: "abcabcddd")
+		XCTAssertEqual(result, 3)
+
+		// String "aaaaaaaaaaaaa", Substring "a" -> 1
+		result = lengthOfLongestSubstring(s: "aaaaaaaaaaaaa")
+		XCTAssertEqual(result, 1)
+
+		// String "pwwkew", Substring "wke" -> 1
+		result = lengthOfLongestSubstring(s: "pwwkew")
+		XCTAssertEqual(result, 3)
+
+		// String "abcdefcabgd" Substring "defcabg" -> 7
+		result = lengthOfLongestSubstring(s: "abcdefcabgd")
+		XCTAssertEqual(result, 7)
+
+		// String "abcdefcabghi" Substring "defcabghi" -> 9
+		result = lengthOfLongestSubstring(s: "abcdefcabgd")
+		XCTAssertEqual(result, 9)
+
+		// String "abcdefcccdehi" Substring "abcdef" -> 6
+		result = lengthOfLongestSubstring(s: "abcdefcabgd")
+		XCTAssertEqual(result, 6)
 	}
 }
