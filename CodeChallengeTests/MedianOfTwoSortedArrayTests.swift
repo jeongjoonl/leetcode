7f9daf4785379f25475d0037d46ae4c2b2c53384
Jeongjoon Lee <joondev32@gmail.com>
2016-08-07 23:20:00 -0700

    Added test case for median of two sorted array problem

--- CodeChallengeTests/MedianOfTwoSortedArrayTests.swift
+++ CodeChallengeTests/MedianOfTwoSortedArrayTests.swift
@@ -10,5 +10,13 @@ import XCTest
 
 class MedianOfTwoSortedArrayTests: XCTestCase {
 	func testFindMedianSortedArrays() {
+		var result = findMedianSortedArrays(nums1: [1,3], nums2: [2])
+		XCTAssertEqual(result, 2.0)
+
+		result = findMedianSortedArrays(nums1: [1,3], nums2: [2, 4])
+		XCTAssertEqual(result, 2.5)
+
+		result = findMedianSortedArrays(nums1: [1,3,4,6,8,10,13], nums2: [2,7,11,23])
+		XCTAssertEqual(result, 7.0)
 	}
 }
