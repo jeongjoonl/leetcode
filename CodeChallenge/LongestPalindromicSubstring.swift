f3e93a86043cead3ddbb49ec198e0ff52b8dac4f
Jeongjoon Lee <joondev32@gmail.com>
2016-08-10 00:51:47 -0700

    Initial setup for longest palindromic substring problems

---
+++ CodeChallenge/LongestPalindromicSubstring.swift
@@ -0,0 +1,17 @@
+//
+//  LongestPalindromicSubstring.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/10/16.
+//
+//
+
+
+/// Finds the longest palindromic substring in string S.
+///
+/// - parameter s: arbitrary string
+///
+/// - returns: longest palindromic substring
+func longestPalindrome(s: String) -> String {
+	return ""
+}
---
+++ CodeChallengeTests/LongestPalindromicSubstringTests.swift
@@ -0,0 +1,15 @@
+//
+//  LongestPalindromicSubstringTests.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/10/16.
+//
+//
+
+import XCTest
+
+class LongestPalindromicSubstringTests: XCTestCase {
+	func testLongestPalindrome() {
+
+	}
+}
