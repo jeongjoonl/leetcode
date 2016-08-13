e7fd528a24441c069e65ad1b467da6664bd03814
Jeongjoon Lee <joondev32@gmail.com>
2016-08-12 22:55:49 -0700

    Initial setup for ZigZag Conversion

---
+++ CodeChallenge/ZigZagConversion.swift
@@ -0,0 +1,25 @@
+//
+//  ZigZagConversion.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/12/16.
+//
+//
+
+/// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+/// (you may want to display this pattern in a fixed font for better legibility)
+/// P   A   H   N
+/// A P L S I I G
+/// Y   I   R
+/// And then read line by line: "PAHNAPLSIIGYIR"
+/// Write the code that will take a string and make this conversion given a number of rows:
+///
+/// convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+///
+/// - parameter s:       <#s description#>
+/// - parameter numRows: <#numRows description#>
+///
+/// - returns: <#return value description#>
+func convert(s: String, _ numRows: Int) -> String {
+	return ""
+}
---
+++ CodeChallengeTests/ZigZag ConversionTests.swift
@@ -0,0 +1,15 @@
+//
+//  ZigZag ConversionTests.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/12/16.
+//
+//
+
+import XCTest
+
+class ZigZag_ConversionTests: XCTestCase {
+	func testConvert() {
+
+	}
+}
