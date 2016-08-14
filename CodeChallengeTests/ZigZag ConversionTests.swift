e624ab81a6bfbc21e7f675cc434cfc6ff2aedf64
Jeongjoon Lee <joondev32@gmail.com>
2016-08-14 15:15:18 -0700

    Test case added for ZigZag conversion

--- CodeChallengeTests/ZigZag ConversionTests.swift
+++ CodeChallengeTests/ZigZag ConversionTests.swift
@@ -10,6 +10,14 @@ import XCTest
 
 class ZigZag_ConversionTests: XCTestCase {
 	func testConvert() {
+		XCTAssertEqual(convert(s: "PAYPALISHIRING", numRows: 3), "PAHNAPLSIIGYIR")
 
+		XCTAssertEqual(convert(s: "abcdefg", numRows: 1), "abcdefg")
+		XCTAssertEqual(convert(s: "abcdefg", numRows: 2), "acegbdf")
+		XCTAssertEqual(convert(s: "abcdefg", numRows: 3), "aebdfcg")
+		XCTAssertEqual(convert(s: "abcdefghij", numRows: 4), "agbfhceidj")
+		XCTAssertEqual(convert(s: "abcdefghijklm", numRows: 5), "aibhjcgkdflem")
+		XCTAssertEqual(convert(s: "abcdefghijklmnop", numRows: 6), "akbjlcimdhnegofp")
+		XCTAssertEqual(convert(s: "abcdefghijklmnopqrs", numRows: 7), "amblnckodjpeiqfhrgs")
 	}
 }
