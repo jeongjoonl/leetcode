70a83cc9a2abb7d6bfcdf43df1ea42d0c87f7597
Jeongjoon Lee <joondev32@gmail.com>
2016-08-14 18:12:53 -0700

    Initial setup for Reverse Integer

---
+++ CodeChallenge/ReverseInteger.swift
@@ -0,0 +1,19 @@
+//
+//  ReverseInteger.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/14/16.
+//
+//
+
+/// Reverse digits of an integer.
+///
+/// Example1: x = 123, return 321
+/// Example2: x = -123, return -321
+///
+/// - parameter x: integer
+///
+/// - returns: reversed digits of integer
+func reverse(x: Int) -> Int {
+	return 0
+}
---
+++ CodeChallengeTests/ReverseIntegerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ReverseIntegerTests.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/14/16.
+//
+//
+
+import XCTest
+
+class ReverseIntegerTests: XCTestCase {
+	func testReverse() {
+
+	}
+}
