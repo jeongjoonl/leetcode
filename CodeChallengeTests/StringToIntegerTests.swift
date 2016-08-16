574da8ea4ecb2a28e2e42af3fdce83bafa907655
Jeongjoon Lee <joondev32@gmail.com>
2016-08-15 23:53:30 -0700

    Added test cases for String to Integer

--- CodeChallengeTests/StringToIntegerTests.swift
+++ CodeChallengeTests/StringToIntegerTests.swift
@@ -10,5 +10,14 @@ import XCTest
 
 class StringToIntegerTests: XCTestCase {
 	func testMyAtoi() {
+		XCTAssertEqual(myAtoi(str: "123"), 123)
+		XCTAssertEqual(myAtoi(str: "-123"), -123)
+		XCTAssertEqual(myAtoi(str: "923912312312326432"), 923912312312326432)
+		XCTAssertEqual(myAtoi(str: "-923912312312326432"), -923912312312326432)
+		XCTAssertEqual(myAtoi(str: "123-"), nil)
+		XCTAssertEqual(myAtoi(str: "a123"), nil)
+		XCTAssertEqual(myAtoi(str: "1b23"), nil)
+		XCTAssertEqual(myAtoi(str: "-123c"), nil)
+		XCTAssertEqual(myAtoi(str: "-12z3"), nil)
 	}
 }
