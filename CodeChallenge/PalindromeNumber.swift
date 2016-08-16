2519301f8817aaa0057ead9befe0790213121680
Jeongjoon Lee <joondev32@gmail.com>
2016-08-16 00:46:54 -0700

    Initial set up for Palindrome Number

---
+++ CodeChallenge/PalindromeNumber.swift
@@ -0,0 +1,17 @@
+//
+//  PalindromeNumber.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/16/16.
+//
+//
+
+/// Determine whether an integer is a palindrome.
+/// Do this without extra space.
+///
+/// - parameter x: integer
+///
+/// - returns: true if integer is a palindrome, otherwise false
+func isPalindrome(x: Int) -> Bool {
+	return true
+}
---
+++ CodeChallengeTests/PalindromeNumberTests.swift
@@ -0,0 +1,14 @@
+//
+//  PalindromeNumberTests.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/16/16.
+//
+//
+
+import XCTest
+
+class PalindromeNumberTests: XCTestCase {
+	func testIsPalindrome() {
+	}
+}
