2985ee526ac73ff757f3cb9d5e9cffd736b831e0
Jeongjoon Lee <joondev32@gmail.com>
2016-08-16 00:34:30 -0700

    Implemented String to Integer

--- CodeChallenge/StringToInteger.swift
+++ CodeChallenge/StringToInteger.swift
@@ -13,6 +13,49 @@
 /// - parameter str: string
 ///
 /// - returns: integer
-func myAtoi(str: String) -> Int {
-	return 0
+func myAtoi(str: String) -> Int? {
+	let len = str.characters.count
+	var result = 0
+	var sign = 1
+	var index = 0
+
+	if str[0] == "-" {
+		sign = -1
+		index += 1
+	}
+
+	while index < len, let digit = Int(String(str[index])) {
+		result = result * 10 + digit
+		index += 1
+	}
+
+	return index < len ? nil : sign * result
+}
+
+private extension String {
+	subscript(i: Int) -> Character {
+		get {
+			return self[self.index(self.startIndex, offsetBy: i)]
+		}
+	}
+}
+
+/// Using closure
+func myAtoi2(str: String) -> Int? {
+	var str = str
+	var sign = 1
+
+	if str[0] == "-" {
+		sign = -1
+		str = str.substring(from: str.index(after: str.startIndex))
+	}
+
+	let result = str.characters.reduce(0) { (acc: Int?, c: Character) -> Int? in
+		if let acc = acc, let digit = Int(String(c)) {
+			return acc * 10 + digit
+		}
+		return nil
+	}
+
+	return result != nil ? sign * result! : nil
 }
