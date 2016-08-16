c69f2a960828bcdd1a2d86ba4ee631bc903022de
Jeongjoon Lee <joondev32@gmail.com>
2016-08-15 18:14:38 -0700

    Initial set up for string to integer problem

---
+++ CodeChallenge/StringToInteger.swift
@@ -0,0 +1,18 @@
+//
+//  StringToInteger.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/15/16.
+//
+//
+
+/// Convert a string to an integer.
+/// Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+/// Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+///
+/// - parameter str: string
+///
+/// - returns: integer
+func myAtoi(str: String) -> Int {
+	return 0
+}
---
+++ CodeChallengeTests/StringToIntegerTests.swift
@@ -0,0 +1,14 @@
+//
+//  StringToIntegerTests.swift
+//  CodeChallenge
+//
+//  Created by Jeongjoon Lee on 8/15/16.
+//
+//
+
+import XCTest
+
+class StringToIntegerTests: XCTestCase {
+	func testMyAtoi() {
+	}
+}
