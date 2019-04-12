e1872a5f86ebacecfd1cbf8e5f05941accb11bbf
Jeongjoon Lee <joondev32@gmail.com>
2019-04-12 15:51:01 -0700

    Binary Tree Postorder Traversal

---
+++ BinaryTreePostorderTraversal/BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,34 @@
+// BinaryTreePostorderTraversal.cpp
+// Created by Jeongjoon Lee on 4/12/2019
+
+// Given a binary tree, return the postorder traversal of its nodes' values.
+// 
+// Example:
+// 
+// Input: [1,null,2,3]
+//    1
+//     \
+//      2
+//     /
+//    3
+// 
+// Output: [3,2,1]
+
+vector<int> postorderTraversal(TreeNode* root)
+{
+	vector<int> result;
+	postorderTraversal(root, result);
+	return result;
+}
+
+void postorderTraversal(TreeNode* node, vector<int>& v)
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+	
+	postorderTraversal(node->left, v);
+	postorderTraversal(node->right, v);
+	v.push_back(node->val);
+}
