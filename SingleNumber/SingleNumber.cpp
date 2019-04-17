78d1fbdcd3ec8976170ed3561389ec25d2d62c13
Jeongjoon Lee <joondev32@gmail.com>
2019-04-17 00:17:40 -0700

    Single Number

---
+++ SingleNumber/SingleNumber.cpp
@@ -0,0 +1,30 @@
+// MinimumAbsoluteDifferenceInBST.cpp
+// Created by Jeongjoon Lee on 4/16/2019.
+
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+//
+// Note:
+//
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+//
+// Example 1:
+//
+// Input: [2,2,1]
+// Output: 1
+//
+// Example 2:
+//
+// Input: [4,1,2,1,2]
+// Output: 4
+
+int singleNumber(vector<int>& nums)
+{
+	int result = 0;
+	
+	for (auto& num : nums)
+	{
+		result ^= num;
+	}
+	
+	return result;
+}
